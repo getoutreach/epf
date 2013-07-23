b9c5f6aff60ab076ca3e64ed6d65f778736aece6
heartsentwined <heartsentwined.me@gmail.com>
2013-07-23 17:43:50 +0800

    source map fix

--- lib/epf/source.rb
+++ lib/epf/source.rb
@@ -3,5 +3,9 @@ module Epf
     def self.bundled_path
       File.expand_path('../../../dist/epf.js', __FILE__)
     end
+
+    def self.bundled_map_path
+      File.expand_path('../../../dist/epf.js.map', __FILE__)
+    end
   end
 end
