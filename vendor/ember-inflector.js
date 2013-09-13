e5507501ecb92283c00890b2bfb17720a8cd1f5f
Gordon L. Hempton <ghempton@gmail.com>
2013-09-13 09:50:22 -0700

    updated ember-inflector

--- vendor/ember-inflector.js
+++ vendor/ember-inflector.js
@@ -1,172 +1,203 @@
 (function() {
-Ember.INFLECTED_CLASSIFY = Ember.ENV.INFLECTED_CLASSIFY;
-
-if (typeof Ember.INFLECTED_CLASSIFY === 'undefined') {
-  Ember.INFLECTED_CLASSIFY = false;
-}
-
-
 Ember.String.pluralize = function(word) {
-  return Ember.Inflector.inflect(word, Ember.Inflector.rules.plurals);
+  return Ember.Inflector.inflector.pluralize(word);
 };
 
 Ember.String.singularize = function(word) {
-  return Ember.Inflector.inflect(word, Ember.Inflector.rules.singular);
+  return Ember.Inflector.inflector.singularize(word);
 };
-
-Ember.String.humanize = function(word) {
-
-  var inflected = Ember.Inflector.inflect(word,Ember.Inflector.rules.humans);
-
-  inflected = inflected.replace(Ember.Inflector.KEY_SUFFIX_REGEX,'');
-  inflected = inflected.replace(Ember.Inflector.WHITESPACE_REGEX,' ');
-  inflected = inflected.replace(/_/g,' ');
-
-  // acronymize?
-
-  return Ember.String.capitalize(inflected);
-};
-
-Ember.String.titleize = function(word) {
-   var result = Ember.String.humanize(word);
-
-   result = result.
-     replace(/\b(?:<!['’`])[a-z]/).
-     toLowerCase().
-     replace(/^.|\s\S/g, function(a) { return a.toUpperCase(); });
-
-  return result;
-};
-
-Ember.String.capitalize = function(word) {
-  return word.replace(Ember.Inflector.FIRST_LETTER_REGEX, function(match) {
-    return match.toUpperCase();
-  });
-};
-
-Ember.String.tableize = function(word) {
-  return Ember.String.pluralize(Ember.String.underscore(word.toLowerCase()));
-};
-
-if (Ember.INFLECTED_CLASSIFY) {
-  Ember.String.classify = function(word) {
-    return Ember.String.capitalize(Ember.String.camelize(Ember.String.singularize(word)));
-  };
-}
 
 })();
 
 
 
 (function() {
-Ember.Inflector = {
-  FIRST_LETTER_REGEX: /^\w/,
-  WHITESPACE_REGEX: /\s+/,
-  KEY_SUFFIX_REGEX: /_id$/,
-  BLANK_REGEX: /^\s*$/,
+var BLANK_REGEX = /^\s*$/;
 
-  _CACHE: {},
-  cache: function(word, rules, value){
-    Ember.Inflector._CACHE[word] = Ember.Inflector._CACHE[word] || {};
+function loadUncountable(rules, uncountable) {
+  for (var i = 0, length = uncountable.length; i < length; i++) {
+    rules.uncountable[uncountable[i]] = true;
+  }
+}
 
-    if (value){
-      Ember.Inflector._CACHE[word][rules] = value;
-    }
+function loadIrregular(rules, irregularPairs) {
+  var pair;
 
-    return Ember.Inflector._CACHE[word][rules];
-  },
+  for (var i = 0, length = irregularPairs.length; i < length; i++) {
+    pair = irregularPairs[i];
 
-  clearCache: function(){
-    Ember.Inflector._CACHE = {};
-  },
+    rules.irregular[pair[0]] = pair[1];
+    rules.irregularInverse[pair[1]] = pair[0];
+  }
+}
 
-  clearRules: function(){
-    Ember.Inflector.rules.plurals     = [];
-    Ember.Inflector.rules.plurals     = [];
-    Ember.Inflector.rules.singular    = [];
-    Ember.Inflector.rules.humans      = [];
-    Ember.Inflector.rules.uncountable = {};
-    Ember.Inflector.rules.irregular   = {};
-    Ember.Inflector.rules.irregularInverse = {};
-  },
+/**
+  Inflector.Ember provides a mechanism for supplying inflection rules for your
+  application. Ember includes a default set of inflection rules, and provides an
+  API for providing additional rules.
 
-  rules: {
-    plurals:  [],
-    singular: [],
-    humans:   [],
+  Examples:
+
+  Creating an inflector with no rules.
+
+  ```js
+  var inflector = new Ember.Inflector();
+  ```
+
+  Creating an inflector with the default ember ruleset.
+
+  ```js
+  var inflector = new Ember.Inflector(Ember.Inflector.defaultRules);
+
+  inflector.pluralize('cow') //=> 'kine'
+  inflector.singularize('kine') //=> 'cow'
+  ```
+
+  Creating an inflector and adding rules later.
+
+  ```javascript
+  var inflector = Ember.Inflector.inflector;
+
+  inflector.pluralize('advice') // => 'advices'
+  inflector.uncountable('advice');
+  inflector.pluralize('advice') // => 'advice'
+
+  inflector.pluralize('formula') // => 'formulas'
+  inflector.irregular('formula', 'formulae');
+  inflector.pluralize('formula') // => 'formulae'
+
+  // you would not need to add these as they are the default rules
+  inflector.plural(/$/, 's');
+  inflector.singular(/s$/i, '');
+  ```
+
+  Creating an inflector with a nondefault ruleset.
+
+  ```javascript
+  var rules = {
+    plurals:  [ /$/, 's' ],
+    singular: [ /\s$/, '' ],
+    irregularPairs: [
+      [ 'cow', 'kine' ]
+    ],
+    uncountable: [ 'fish' ]
+  };
+
+  var inflector = new Ember.Inflector(rules);
+  ```
+
+  @class Inflector
+  @namespace Ember
+*/
+function Inflector(ruleSet) {
+  ruleSet = ruleSet || {};
+  ruleSet.uncountable = ruleSet.uncountable || {};
+  ruleSet.irregularPairs= ruleSet.irregularPairs|| {};
+
+  var rules = this.rules = {
+    plurals:  ruleSet.plurals || [],
+    singular: ruleSet.singular || [],
     irregular: {},
     irregularInverse: {},
     uncountable: {}
+  };
+
+  loadUncountable(rules, ruleSet.uncountable);
+  loadIrregular(rules, ruleSet.irregularPairs);
+}
+
+Inflector.prototype = {
+  /**
+    @method plural
+    @param {RegExp} regex
+    @param {String} string
+  */
+  plural: function(regex, string) {
+    this.rules.plurals.push([regex, string]);
   },
 
-  reset: function(){
-    Ember.Inflector.clearCache();
-    Ember.Inflector.clearRules();
+  /**
+    @method singular
+    @param {RegExp} regex
+    @param {String} string
+  */
+  singular: function(regex, string) {
+    this.rules.singular.push([regex, string]);
   },
 
-  plural: function(rule,substituion){
-    Ember.Inflector.rules.plurals.addObject([rule, substituion]);
+  /**
+    @method uncountable
+    @param {String} regex
+  */
+  uncountable: function(string) {
+    loadUncountable(this.rules, [string]);
   },
 
-  singular: function(rule,substituion){
-    Ember.Inflector.rules.singular.addObject([rule, substituion]);
+  /**
+    @method irregular
+    @param {String} singular
+    @param {String} plural
+  */
+  irregular: function (singular, plural) {
+    loadIrregular(this.rules, [[singular, plural]]);
   },
 
-  human: function(rule,substituion){
-    Ember.Inflector.rules.humans.addObject([rule, substituion]);
+  /**
+    @method pluralize
+    @param {String} word
+  */
+  pluralize: function(word) {
+    return this.inflect(word, this.rules.plurals);
   },
 
-  irregular: function(rule,substituion){
-    Ember.Inflector.rules.irregular[rule] = substituion;
-    Ember.Inflector.rules.irregularInverse[substituion] = rule;
+  /**
+    @method singularize
+    @param {String} word
+  */
+  singularize: function(word) {
+    return this.inflect(word, this.rules.singular);
   },
 
-  uncountable: function(uncountable) {
-    uncountable.forEach(function(word) {
-      Ember.Inflector.rules.uncountable[word] = true;
-    });
-  },
+  /**
+    @method inflect
+    @param {String} word
+    @param {Object} typeRules
+  */
+  inflect: function(word, typeRules) {
+    var inflection, substitution, result, lowercase, isBlank,
+    isUncountable, isIrregular, isIrregularInverse, rule;
 
-  inflect: function(word, rules) {
-    var inflection, substitution, result, lowercase,
-    isCached, isIrregular, isIrregularInverse, rule;
+    isBlank = BLANK_REGEX.test(word);
 
-    if (Ember.Inflector.BLANK_REGEX.test(word)){
+    if (isBlank) {
       return word;
     }
 
     lowercase = word.toLowerCase();
 
-    isCached =  Ember.Inflector.cache(lowercase,rules);
-    if (isCached){
-      // cached
-      return isCached;
-    }
+    isUncountable = this.rules.uncountable[lowercase];
 
-    if (Ember.Inflector.rules.uncountable[lowercase]){
-      // uncountable
+    if (isUncountable) {
       return word;
     }
 
-    isIrregular = Ember.Inflector.rules.irregular[lowercase];
+    isIrregular = this.rules.irregular[lowercase];
 
-    if (isIrregular){
-      // irregular
+    if (isIrregular) {
       return isIrregular;
     }
 
-    isIrregularInverse = Ember.Inflector.rules.irregularInverse[lowercase];
+    isIrregularInverse = this.rules.irregularInverse[lowercase];
 
-    if (isIrregularInverse){
-      // irregular
+    if (isIrregularInverse) {
       return isIrregularInverse;
     }
 
-    for(var i = rules.length, min = 0; i > min; i--){
-      inflection = rules[i-1],
-      rule = inflection[0];
+    for (var i = typeRules.length, min = 0; i > min; i--) {
+       inflection = typeRules[i-1];
+       rule = inflection[0];
 
-      if(rule.test(word)){
+      if (rule.test(word)) {
         break;
       }
     }
@@ -176,116 +207,96 @@ Ember.Inflector = {
     rule = inflection[0];
     substitution = inflection[1];
 
-    result = word.replace(rule,substitution);
+    result = word.replace(rule, substitution);
 
-    Ember.Inflector.cache(lowercase,rules,result);
     return result;
-
   }
 };
 
-})();
-
-
-
-(function() {
-Ember.Inflector.loadAll = function(){
-  Ember.Inflector.plural(/$/, 's');
-  Ember.Inflector.plural(/s$/i, 's');
-  Ember.Inflector.plural(/^(ax|test)is$/i, '$1es');
-  Ember.Inflector.plural(/(octop|vir)us$/i, '$1i');
-  Ember.Inflector.plural(/(octop|vir)i$/i, '$1i');
-  Ember.Inflector.plural(/(alias|status)$/i, '$1es');
-  Ember.Inflector.plural(/(bu)s$/i, '$1ses');
-  Ember.Inflector.plural(/(buffal|tomat)o$/i, '$1oes');
-  Ember.Inflector.plural(/([ti])um$/i, '$1a');
-  Ember.Inflector.plural(/([ti])a$/i, '$1a');
-  Ember.Inflector.plural(/sis$/i, 'ses');
-  Ember.Inflector.plural(/(?:([^f])fe|([lr])f)$/i, '$1$2ves');
-  Ember.Inflector.plural(/(hive)$/i, '$1s');
-  Ember.Inflector.plural(/([^aeiouy]|qu)y$/i, '$1ies');
-  Ember.Inflector.plural(/(x|ch|ss|sh)$/i, '$1es');
-  Ember.Inflector.plural(/(matr|vert|ind)(?:ix|ex)$/i, '$1ices');
-  Ember.Inflector.plural(/^(m|l)ouse$/i, '$1ice');
-  Ember.Inflector.plural(/^(m|l)ice$/i, '$1ice');
-  Ember.Inflector.plural(/^(ox)$/i, '$1en');
-  Ember.Inflector.plural(/^(oxen)$/i, '$1');
-  Ember.Inflector.plural(/(quiz)$/i, '$1zes');
-
-  Ember.Inflector.singular(/s$/i, '');
-  Ember.Inflector.singular(/(ss)$/i, '$1');
-  Ember.Inflector.singular(/(n)ews$/i, '$1ews');
-  Ember.Inflector.singular(/([ti])a$/i, '$1um');
-  Ember.Inflector.singular(/((a)naly|(b)a|(d)iagno|(p)arenthe|(p)rogno|(s)ynop|(t)he)(sis|ses)$/i, '$1sis');
-  Ember.Inflector.singular(/(^analy)(sis|ses)$/i, '$1sis');
-  Ember.Inflector.singular(/([^f])ves$/i, '$1fe');
-  Ember.Inflector.singular(/(hive)s$/i, '$1');
-  Ember.Inflector.singular(/(tive)s$/i, '$1');
-  Ember.Inflector.singular(/([lr])ves$/i, '$1f');
-  Ember.Inflector.singular(/([^aeiouy]|qu)ies$/i, '$1y');
-  Ember.Inflector.singular(/(s)eries$/i, '$1eries');
-  Ember.Inflector.singular(/(m)ovies$/i, '$1ovie');
-  Ember.Inflector.singular(/(x|ch|ss|sh)es$/i, '$1');
-  Ember.Inflector.singular(/^(m|l)ice$/i, '$1ouse');
-  Ember.Inflector.singular(/(bus)(es)?$/i, '$1');
-  Ember.Inflector.singular(/(o)es$/i, '$1');
-  Ember.Inflector.singular(/(shoe)s$/i, '$1');
-  Ember.Inflector.singular(/(cris|test)(is|es)$/i, '$1is');
-  Ember.Inflector.singular(/^(a)x[ie]s$/i, '$1xis');
-  Ember.Inflector.singular(/(octop|vir)(us|i)$/i, '$1us');
-  Ember.Inflector.singular(/(alias|status)(es)?$/i, '$1');
-  Ember.Inflector.singular(/^(ox)en/i, '$1');
-  Ember.Inflector.singular(/(vert|ind)ices$/i, '$1ex');
-  Ember.Inflector.singular(/(matr)ices$/i, '$1ix');
-  Ember.Inflector.singular(/(quiz)zes$/i, '$1');
-  Ember.Inflector.singular(/(database)s$/i, '$1');
-
-  Ember.Inflector.irregular('person', 'people');
-  Ember.Inflector.irregular('man', 'men');
-  Ember.Inflector.irregular('child', 'children');
-  Ember.Inflector.irregular('sex', 'sexes');
-  Ember.Inflector.irregular('move', 'moves');
-  Ember.Inflector.irregular('cow', 'kine');
-  Ember.Inflector.irregular('zombie', 'zombies');
-
-  Ember.Inflector.uncountable("equipment information rice money species series fish sheep jeans police".w());
-};
+Ember.Inflector = Inflector;
 
 })();
 
 
 
 (function() {
-Ember.Inflector.rules.ordinalization = {
-  'default': 'th',
-  0:  '',
-  1:  'st',
-  2:  'nd',
-  3:  'rd',
-  11: 'th',
-  12: 'th',
-  13: 'th'
-};
+Ember.Inflector.defaultRules = {
+  plurals: [
+    [/$/, 's'],
+    [/s$/i, 's'],
+    [/^(ax|test)is$/i, '$1es'],
+    [/(octop|vir)us$/i, '$1i'],
+    [/(octop|vir)i$/i, '$1i'],
+    [/(alias|status)$/i, '$1es'],
+    [/(bu)s$/i, '$1ses'],
+    [/(buffal|tomat)o$/i, '$1oes'],
+    [/([ti])um$/i, '$1a'],
+    [/([ti])a$/i, '$1a'],
+    [/sis$/i, 'ses'],
+    [/(?:([^f])fe|([lr])f)$/i, '$1$2ves'],
+    [/(hive)$/i, '$1s'],
+    [/([^aeiouy]|qu)y$/i, '$1ies'],
+    [/(x|ch|ss|sh)$/i, '$1es'],
+    [/(matr|vert|ind)(?:ix|ex)$/i, '$1ices'],
+    [/^(m|l)ouse$/i, '$1ice'],
+    [/^(m|l)ice$/i, '$1ice'],
+    [/^(ox)$/i, '$1en'],
+    [/^(oxen)$/i, '$1'],
+    [/(quiz)$/i, '$1zes']
+  ],
 
-Ember.Inflector.ordinal = function(number) {
-  number = parseInt(number,10);
-  number = Math.abs(number);
+  singular: [
+    [/s$/i, ''],
+    [/(ss)$/i, '$1'],
+    [/(n)ews$/i, '$1ews'],
+    [/([ti])a$/i, '$1um'],
+    [/((a)naly|(b)a|(d)iagno|(p)arenthe|(p)rogno|(s)ynop|(t)he)(sis|ses)$/i, '$1sis'],
+    [/(^analy)(sis|ses)$/i, '$1sis'],
+    [/([^f])ves$/i, '$1fe'],
+    [/(hive)s$/i, '$1'],
+    [/(tive)s$/i, '$1'],
+    [/([lr])ves$/i, '$1f'],
+    [/([^aeiouy]|qu)ies$/i, '$1y'],
+    [/(s)eries$/i, '$1eries'],
+    [/(m)ovies$/i, '$1ovie'],
+    [/(x|ch|ss|sh)es$/i, '$1'],
+    [/^(m|l)ice$/i, '$1ouse'],
+    [/(bus)(es)?$/i, '$1'],
+    [/(o)es$/i, '$1'],
+    [/(shoe)s$/i, '$1'],
+    [/(cris|test)(is|es)$/i, '$1is'],
+    [/^(a)x[ie]s$/i, '$1xis'],
+    [/(octop|vir)(us|i)$/i, '$1us'],
+    [/(alias|status)(es)?$/i, '$1'],
+    [/^(ox)en/i, '$1'],
+    [/(vert|ind)ices$/i, '$1ex'],
+    [/(matr)ices$/i, '$1ix'],
+    [/(quiz)zes$/i, '$1'],
+    [/(database)s$/i, '$1']
+  ],
 
-  if (number > 10 && number < 14){
-    number %= 100;
-  } else {
-    number %= 10;
-  }
+  irregularPairs: [
+    ['person', 'people'],
+    ['man', 'men'],
+    ['child', 'children'],
+    ['sex', 'sexes'],
+    ['move', 'moves'],
+    ['cow', 'kine'],
+    ['zombie', 'zombies']
+  ],
 
-  var ordinalization = Ember.Inflector.rules.ordinalization;
-
-  return ordinalization[number] || ordinalization['default'];
-};
-
-Ember.String.ordinalize = function (word) {
-  var ordinalization = Ember.Inflector.ordinal(word);
-
-  return [word, ordinalization].join('');
+  uncountable: [
+    'equipment',
+    'information',
+    'rice',
+    'money',
+    'species',
+    'series',
+    'fish',
+    'sheep',
+    'jeans',
+    'police'
+  ]
 };
 
 })();
@@ -293,64 +304,26 @@ Ember.String.ordinalize = function (word) {
 
 
 (function() {
-var pluralize = Ember.String.pluralize,
-    singularize = Ember.String.singularize,
-    humanize = Ember.String.humanize,
-    titleize = Ember.String.titleize,
-    capitalize = Ember.String.capitalize,
-    tableize = Ember.String.tableize,
-    classify = Ember.String.classify;
-
 if (Ember.EXTEND_PROTOTYPES) {
-    
-    /*
-     * 
-     */
-    String.prototype.pluralize = function() {
-       return pluralize(this, arguments);
-    };
+  /**
+    See {{#crossLink "Ember.String/pluralize"}}{{/crossLink}}
 
-    /*
-     * 
-     */
-    String.prototype.singularize = function() {
-       return singularize(this, arguments);
-    };
+    @method pluralize
+    @for String
+  */
+  String.prototype.pluralize = function() {
+    return Ember.String.pluralize(this);
+  };
 
-    /*
-     * 
-     */
-    String.prototype.humanize = function() {
-       return humanize(this, arguments);
-    };
+  /**
+    See {{#crossLink "Ember.String/singularize"}}{{/crossLink}}
 
-    /*
-     * 
-     */
-    String.prototype.titleize = function() {
-       return titleize(this, arguments);
-    };
-
-    /*
-     * 
-     */
-    String.prototype.capitalize = function() {
-       return capitalize(this, arguments);
-    };
-
-    /*
-     * 
-     */
-    String.prototype.tableize = function() {
-       return tableize(this, arguments);
-    };
-
-    /*
-     * 
-     */
-    String.prototype.classify = function() {
-       return classify(this, arguments);
-    };
+    @method singularize
+    @for String
+  */
+  String.prototype.singularize = function() {
+    return Ember.String.singularize(this);
+  };
 }
 
 })();
@@ -358,6 +331,7 @@ if (Ember.EXTEND_PROTOTYPES) {
 
 
 (function() {
+Ember.Inflector.inflector = new Ember.Inflector(Ember.Inflector.defaultRules);
 
 })();
 
